1d733b5f37b560118a54f2c266eecdb5eb6c68f3
SoraKumo <info@croud.jp>
2023-09-05 22:14:45 +0900

    change playground

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,10 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { builder } from './libs/builder';
 import { ConfigModule } from '@nestjs/config';
+import { prisma } from './libs/context';
+import jsonwebtoken from 'jsonwebtoken';
+import { Request, Response } from 'express';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 
 const schema = builder.toSchema();
 
@@ -10,9 +14,29 @@ const schema = builder.toSchema();
   imports: [
     ConfigModule.forRoot(),
     GraphQLModule.forRoot<ApolloDriverConfig>({
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      playground: false,
       driver: ApolloDriver,
       schema,
-      context: ({ req }) => ({ req }),
+      context: async ({ req, res }: { req: Request; res: Response }) => {
+        const token = req.cookies?.session;
+        const user = token
+          ? await new Promise<{ id: string; name: string } | undefined>(
+              (resolve) => {
+                jsonwebtoken.verify(token, 'test', (_, data) => {
+                  resolve(
+                    typeof data === 'object'
+                      ? (data?.payload?.user as
+                          | { id: string; name: string }
+                          | undefined)
+                      : undefined,
+                  );
+                });
+              },
+            )
+          : undefined;
+        return { req, res, prisma, user };
+      },
     }),
   ],
 })
